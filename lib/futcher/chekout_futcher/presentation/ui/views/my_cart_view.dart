ddde00e70b6b32afe5e9000754f29cc5ecf83c8b
elshaarawy hassan <elshaarawyhassan7@gmail.com>
2025-08-27 14:37:17 +0300

    create page cart view

---
+++ lib/futcher/chekout_futcher/presentation/ui/views/my_cart_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MyCartView extends StatelessWidget {
+  const MyCartView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white, 
+      appBar: AppBar(),
+    );
+  }
+}

863132f8457b69c456a26c0978b5869ad23e2655
elshaarawy hassan <elshaarawyhassan7@gmail.com>
2025-08-27 12:44:00 +0300

    edit name app

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MarcketApp());
+}
+
+class MarcketApp extends StatelessWidget {
+  const MarcketApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+    );
+  }
+}

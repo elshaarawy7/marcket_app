2b5adb793c3a8a420e5c682161b46ab094a4dc63
elshaarawy hassan <elshaarawyhassan7@gmail.com>
2025-08-27 15:53:35 +0300

    creatae widgets my cart view body and add image

--- lib/futcher/chekout_futcher/presentation/ui/views/my_cart_view.dart
+++ lib/futcher/chekout_futcher/presentation/ui/views/my_cart_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:market_app/futcher/chekout_futcher/presentation/ui/views/widgets/my_cart_view_body.dart';
 
 class MyCartView extends StatelessWidget {
   const MyCartView({super.key});
@@ -6,20 +7,22 @@ class MyCartView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white, 
       appBar: AppBar(
+      backgroundColor: Colors.white, 
         title: Text("My Cart" , style: TextStyle(
           color: Colors.black , 
           fontSize: 20 , 
           fontWeight: FontWeight.bold , 
-        ),)  
+        ),)  , 
+        centerTitle: true,
+
+        leading: Icon(Icons.arrow_back , color: Colors.black, size: 40,), 
       
       ), 
-      body: Column(
-        children: [
-          
-        ],
-      ),
+      body:Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: MyCartViewBody(),
+      )
     );
   }
 }
---
+++ lib/futcher/chekout_futcher/presentation/ui/views/widgets/my_cart_view_body.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class MyCartViewBody extends StatelessWidget {
+  const MyCartViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: [
+          Image.asset("assets/images/Group 6.png" , height: 500, width: 400,)
+        ],
+      ),
+    );
+  }
+}

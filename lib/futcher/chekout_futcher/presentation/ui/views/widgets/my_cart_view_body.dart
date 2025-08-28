ccf96600ec92dfcb85d1db3b3c95cf24200dcfd7
elshaarawy hassan <elshaarawyhassan7@gmail.com>
2025-08-28 11:08:59 +0300

    add divider

--- lib/futcher/chekout_futcher/presentation/ui/views/widgets/my_cart_view_body.dart
+++ lib/futcher/chekout_futcher/presentation/ui/views/widgets/my_cart_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:market_app/futcher/chekout_futcher/presentation/ui/views/widgets/item_widgets.dart';
 
 class MyCartViewBody extends StatelessWidget {
   const MyCartViewBody({super.key});
@@ -8,7 +9,16 @@ class MyCartViewBody extends StatelessWidget {
     return Center(
       child: Column(
         children: [
-          Image.asset("assets/images/Group 6.png" , height: 500, width: 400,)
+          Image.asset("assets/images/Group 6.png" , height: 500, width: 400,) , 
+          SizedBox(height: 20,) , 
+          ItemWidgets() , 
+          SizedBox(height: 5,) , 
+          Divider( 
+            color: Colors.grey, 
+            height: 32,  
+          ) , 
+          SizedBox(height: 10,) , 
+          
         ],
       ),
     );

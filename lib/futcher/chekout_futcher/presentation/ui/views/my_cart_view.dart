99b8c4b6d5dfc48849191678462d7352cdc9cd21
elshaarawy hassan <elshaarawyhassan7@gmail.com>
2025-08-28 11:48:02 +0300

    create pymant detiles page

--- lib/futcher/chekout_futcher/presentation/ui/views/my_cart_view.dart
+++ lib/futcher/chekout_futcher/presentation/ui/views/my_cart_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:market_app/core/utils/widgets/custem_appar.dart';
 import 'package:market_app/futcher/chekout_futcher/presentation/ui/views/widgets/my_cart_view_body.dart';
 
 class MyCartView extends StatelessWidget {
@@ -7,18 +8,11 @@ class MyCartView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        title: Text(
-          "My Cart",
-          style: TextStyle(
-            color: Colors.black,
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        centerTitle: true,
-        leading: Icon(Icons.arrow_back, color: Colors.black, size: 40),
+      appBar: customAppBar(
+        title: "My Cart" , 
+        onBack: () {
+          
+        },
       ),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 16),
---
+++ lib/futcher/chekout_futcher/presentation/ui/views/pymant_detils.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class PymantDetils extends StatelessWidget {
+  const PymantDetils({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 12), 
+    );
+  }
+}
